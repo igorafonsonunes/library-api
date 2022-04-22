ca6e35b3d5ae87f0fda3e3b63a8b8f2b816f7624
Igor Afonso Nunes <igorafonsonunes@outlook.com>
2022-04-22 10:18:21 -0300

    fix: remove foreign key.

--- db/3 - adress.sql
+++ db/3 - adress.sql
@@ -10,8 +10,4 @@ CREATE TABLE adress(
     user_library_id INT,
     created_at timestamp DEFAULT current_timestamp,
     updated_at timestamp DEFAULT current_timestamp,
-    FOREIGN KEY (library_id)
-        REFERENCES library(id),
-    FOREIGN KEY (user_library_id)
-        REFERENCES user_library(id)
 );
