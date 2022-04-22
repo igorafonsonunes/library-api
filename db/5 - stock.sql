402840a98385305b8ce12c2e681358824306d0e6
Igor Afonso Nunes <igorafonsonunes@outlook.com>
2022-04-22 16:06:14 -0300

    fix: add foreign key to stock.

--- db/5 - stock.sql
+++ db/5 - stock.sql
@@ -1,5 +1,6 @@
 CREATE TABLE stock(
     id INT PRIMARY KEY,
+    library_id INT NOT NULL,
     book_id INT NOT NULL,
     number_avaible_books INT NOT NULL,
     total_books INT NOT NULL,
@@ -7,5 +8,7 @@ CREATE TABLE stock(
     created_at timestamp DEFAULT current_timestamp,
     updated_at timestamp DEFAULT current_timestamp,
     FOREIGN KEY (book_id)
-        REFERENCES book(id)
+        REFERENCES book(id),
+    FOREIGN KEY (library_id)
+        REFERENCES library(id)
 );
