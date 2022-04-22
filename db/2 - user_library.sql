5a6bae429a9c7fd8d5a22a464b6f6ecbb6e1ef0f
Igor Afonso Nunes <igorafonsonunes@outlook.com>
2022-04-22 09:24:07 -0300

    feature: change name of sql files.

--- db/2 - user_library.sql
+++ db/2 - user_library.sql
@@ -1,7 +1,6 @@
 CREATE TABLE user_library(
     id INT PRIMARY KEY,
     library_id INT,
-    adress_id INT NOT NULL,
     name VARCHAR(255) NOT NULL,
     cpf VARCHAR(11) UNIQUE NOT NULL,
     email VARCHAR(100) UNIQUE NOT NULL,
@@ -12,7 +11,5 @@ CREATE TABLE user_library(
     created_at timestamp DEFAULT current_timestamp,
     updated_at timestamp DEFAULT current_timestamp,
     FOREIGN KEY (library_id)
-        REFERENCES library(id),
-    FOREIGN KEY (adress_id)
-        REFERENCES adress(id)
+        REFERENCES library(id)
 );
--- db/3 - adress.sql
+++ db/3 - adress.sql
@@ -6,6 +6,12 @@ CREATE TABLE adress(
     complement VARCHAR(255) NOT NULL,
     state VARCHAR(25) NOT NULL,
     uf VARCHAR(2) NOT NULL,
+    library_id INT,
+    user_library_id INT,
     created_at timestamp DEFAULT current_timestamp,
-    updated_at timestamp DEFAULT current_timestamp
+    updated_at timestamp DEFAULT current_timestamp,
+    FOREIGN KEY (library_id)
+        REFERENCES library(id),
+    FOREIGN KEY (user_library_id)
+        REFERENCES user_library(id)
 );
