49e1b935b5f94033e12011e8a9ff445eb00e3739
Igor Afonso Nunes <igorafonsonunes@outlook.com>
2022-04-22 14:05:37 -0300

    feature: add config for ormconfig.json

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,19 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { ConfigModule } from '@nestjs/config';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { LibrariesModule } from './libraries/libraries.module';
+import { AdressesModule } from './adresses/adresses.module';
+import { UsersModule } from './users/users.module';
 
 @Module({
-  imports: [],
+  imports: [
+    ConfigModule.forRoot(), 
+    TypeOrmModule.forRoot(), 
+    LibrariesModule, 
+    AdressesModule, UsersModule
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
